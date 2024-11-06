f5fd6e7bc702046a37f405d0c796b0f575f31f6b
KRITIKA <144862402+Kritika1129@users.noreply.github.com>
2024-11-06 19:41:05 +0530

    Add files via upload

---
+++ using a module.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+# Add the directory containing math_utils.py to the Python path
+sys.path.append("C:\Users\Kritika\OneDrive\Desktop\Python Code\math utils.py")
+
+# Now you can import the math_utils module
+import math_utils
+
+result = math_utils.add(5, 3)
+print(result)
+print("This program is written by KRITIKA ERP-067")
